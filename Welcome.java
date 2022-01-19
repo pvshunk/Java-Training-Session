d13c1a2e2fb97678be6e0a46bd52195b2180f879
pvshunk <66204496+pvshunk@users.noreply.github.com>
2022-01-19 21:48:47 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to first Training session of Java");
+
+	}
+
+}
